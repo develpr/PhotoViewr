d876fa6d1c587b0cf09a13212a2cf8cd221e050e
develpr <kevin@develpr.com>
2013-07-18 20:06:43 -0700

    Initial Commit

---
+++ Photo Viewr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo Viewr
+//
+//  Created by Kevin Mitchell on 7/18/13.
+//  Copyright (c) 2013 Kevin Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Viewr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Photo Viewr
+//
+//  Created by Kevin Mitchell on 7/18/13.
+//  Copyright (c) 2013 Kevin Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
