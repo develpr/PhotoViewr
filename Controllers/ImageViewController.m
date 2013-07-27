d4827e328321cf8ff6091f259f7208ca5b2ab02a
develpr <kevin@develpr.com>
2013-07-27 09:55:40 -0700

    Blocking in place, but not working with centering image

--- Controllers/ImageViewController.m
+++ Controllers/ImageViewController.m
@@ -29,35 +29,47 @@
     return _imageView;
 }
 
-- (void) setImageURL:(NSURL *)imageURL
-{
-    _imageURL = imageURL;
-    [self setupImage];
-}
+    - (void) setImageURL:(NSURL *)imageURL
+    {
+        _imageURL = imageURL;
+        [self setupImage];
+    }
 
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
-{
-    return self.imageView;
-}
+    - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+    {
+        return self.imageView;
+    }
 
-- (void) setupImage
-{
-    if(self.imageView){
-        self.scrollView.contentSize = CGSizeZero;
-        self.imageView.image = nil;
-        
-        NSData *imageData = [[NSData alloc]initWithContentsOfURL:self.imageURL];
-        UIImage *image = [[UIImage alloc] initWithData:imageData];
-        if(image){
-            self.scrollView.contentSize = image.size;
-            self.imageView.image = image;
-            self.imageView.frame = CGRectMake(0,0, image.size.width, image.size.height);
+    - (void) setupImage
+    {
+        if(self.imageView){
+            self.scrollView.contentSize = CGSizeZero;
+            self.imageView.image = nil;
+            
+            NSURL *imageURL = self.imageURL;
+            
+            dispatch_queue_t imageFetchQ = dispatch_queue_create("image fetcher", NULL);
+            dispatch_async(imageFetchQ, ^{
+                NSData *imageData = [[NSData alloc]initWithContentsOfURL:self.imageURL];
+                UIImage *image = [[UIImage alloc] initWithData:imageData];
+                if(self.imageURL == imageURL){
+                    if(image){
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            self.scrollView.contentSize = image.size;
+                            self.imageView.image = image;
+                            self.imageView.frame = CGRectMake(0,0, image.size.width, image.size.height);
+                        });
+                    }
+                }
+                
+            });
         }
     }
-}
 
 - (void) viewDidLayoutSubviews
 {
+    [super viewDidLayoutSubviews];
+    
     CGRect scrollViewBounds = self.scrollView.bounds;
     
     CGFloat scrollAspectRatio = scrollViewBounds.size.width/scrollViewBounds.size.height;
